015da0834b01be5557866da4c27dcdded4d85b9b
zalish-shawon <zalish.shawon@gmail.com>
2023-11-24 03:11:08 +0600

    banner section added

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import MainLayout from './components/MainLayout/MainLayout.jsx';
+import Home from './components/Home/Home.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
+    children: [
+      {  
+        path: "/",
+        element: <Home></Home>
+      }
+  ]
   },
 ]);
 
